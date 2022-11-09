eb3e7f525217cca524abbd9d570c62d8c5f4e195
unknown <faisal.amanzai@tekschool.us>
2022-11-08 18:52:26 -0800

    Branching Practice

---
+++ src/JavaGitPackage/BranchingGitPractice.java
@@ -0,0 +1,10 @@
+package JavaGitPackage;
+
+public class BranchingGitPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is practice git branching");
+	}
+
+}
